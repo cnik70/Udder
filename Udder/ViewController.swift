75448bb7288880eb9d7c8a35816d31555c7c3ccb
Ries, Barret D <barret.ries@thermofisher.com>
2019-02-27 08:38:42 -0500

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Ries, Barret D. on 2/27/19.
+//  Copyright © 2019 Ries, Barret D. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
